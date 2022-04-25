4fa7bc3f2b4e9b8349e22c3edbbbb5397be02ec4
elizaveta.s <easolomyannik@icloud.com>
2022-04-25 20:04:40 +0300

    Update brain-calc and brain-even

--- src/calc.js
+++ src/calc.js
@@ -1,40 +1,34 @@
-import readlineSync from 'readline-sync';
-
 import {
-  randomNumber, randomExpression, wrongAnswer, getAnswer,
+  randomNumber, randomExpression, check, getAnswer, getName,
 } from './index.js';
 
 const calc = () => {
-  console.log('Welcome to the Brain Games!');
-  const name = readlineSync.question('May I have your name? ');
-  console.log('Hello, ' + name + '!');
+  const name = String(getName());
   console.log('What is the result of the expression?');
   for (let i = 0; i < 3; i += 1) {
     const expression = [randomNumber(), randomExpression(), randomNumber()];
     console.log('Question: ', expression.join(' '));
-    let result;
+    let def;
     const operation = expression[1];
     switch (operation) {
       case '+':
-        result = expression[0] + expression[2];
+        def = expression[0] + expression[2];
         break;
       case '-':
-        result = expression[0] - expression[2];
+        def = expression[0] - expression[2];
         break;
       case '*':
-        result = expression[0] * expression[2];
+        def = expression[0] * expression[2];
         break;
       case '/':
-        result = expression[0] / expression[2];
+        def = expression[0] / expression[2];
         break;
       default:
         break;
     }
     const usr = Number(getAnswer());
-    if (usr === result) {
-      console.log('Correct!');
-    } else {
-      wrongAnswer(usr, result, name);
+    console.log(check(usr, def, name));
+    if (check(usr, def, name) !== 'Correct!') {
       return;
     }
   }
--- src/is-even.js
+++ src/is-even.js
@@ -1,25 +1,22 @@
-import readlineSync from 'readline-sync';
-import { getAnswer, isEven, wrongAnswer } from './index.js';
+import {
+  getAnswer, getName, isEven, check,
+} from './index.js';
 
 const evenGame = () => {
-  console.log('Welcome to the Brain Games!');
-  const name = readlineSync.question('May I have your name? ');
-  console.log('Hello, ' + name + '!'); // использую +, чтобы не было лишних пробелов
+  const name = String(getName());
   console.log('Answer "yes" if the number is even, otherwise answer "no".');
   for (let i = 0; i < 3; i += 1) {
     const number = Math.round(Math.random() * 20); // без умножения слишком маленькие числа
     console.log('Question:', number);
-    const answer = getAnswer();
-    let CorrectAnswer;
+    const usr = getAnswer();
+    let def;
     if (isEven(number) === true) {
-      CorrectAnswer = 'yes';
+      def = 'yes';
     } else {
-      CorrectAnswer = 'no';
+      def = 'no';
     }
-    if (answer === CorrectAnswer) {
-      console.log('Correct!');
-    } else {
-      wrongAnswer(answer, CorrectAnswer, name);
+    console.log(check(usr, def, name));
+    if (check(usr, def, name) !== 'Correct!') {
       return;
     }
   }
