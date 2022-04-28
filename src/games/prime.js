adfd911b4024a2ed3d8b98a3a277d096fe7f79d1
elizaveta.s <easolomyannik@icloud.com>
2022-04-28 19:47:47 +0300

    Update brain-prime

--- src/games/prime.js
+++ src/games/prime.js
@@ -1,21 +1,32 @@
-import {
-  getAnswer, getName, check, randomNumber, isPrime, Congratulations,
-} from '../index.js';
+import run from '../index.js';
 
-const primeGame = () => {
-  const name = String(getName());
-  console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
-  for (let i = 0; i < 3; i += 1) {
-    const number = randomNumber();
-    console.log('Question:', number);
-    const def = String(isPrime(number));
-    const usr = getAnswer();
-    console.log(check(usr, def, name));
-    if (check(usr, def, name) !== 'Correct!') {
-      return;
+const randomInteger = (min, max) => {
+  Math.ceil(min);
+  Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+const isPrime = (n) => {
+  if (n < 2) {
+    return 'no';
+  }
+  for (let i = 2; i < n; i += 1) {
+    if (n % i === 0) {
+      return 'no';
     }
   }
-  console.log(Congratulations(name));
+  return 'yes';
+};
+
+const getRound = () => {
+  const question = randomInteger(1, 1000);
+  const answer = isPrime(question);
+  return [question, answer.toString()];
+};
+
+const primeGame = () => {
+  const gameDescription = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+  run(gameDescription, getRound);
 };
 
 export default primeGame;
